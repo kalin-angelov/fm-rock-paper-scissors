56af601a86811cf393a2b63e2eae81ce7c535d7c
Kalizar <kalinangelov@abv.bg>
2024-05-18 19:02:01 +0300

    Create Rules.jsx

---
+++ src/components/Rules.jsx
@@ -0,0 +1,17 @@
+const Rules = () => {
+  return (
+    <section className="rules-section">
+        <h1>Rules</h1>
+        <figure className="rules-explanation">
+            <img src="/public/images/image-rules.svg" alt="rules" />
+        </figure>
+        <button>
+            <figure>
+                <img src="/public/images/icon-close.svg" alt="close-btn" />
+            </figure>
+        </button>
+    </section>
+  );
+};
+
+export default Rules;
