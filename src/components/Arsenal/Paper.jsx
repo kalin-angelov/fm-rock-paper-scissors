6818c059c2b5e76a62cbab989a3f43dcf008b6bf
Kalizar <kalinangelov@abv.bg>
2024-05-19 18:41:44 +0300

    Create Paper.jsx

---
+++ src/components/Arsenal/Paper.jsx
@@ -0,0 +1,11 @@
+const Paper = ({ onSelect }) => {
+  return (
+    <button onClick={ () => onSelect("paper") } id="weapon-of-war-paper" className="weapon-btn" value="paper">
+      <figure id="paper-container" className="weapon-container">
+        <img src="/public/images/icon-paper.svg" alt="paper" />
+      </figure>
+    </button>
+  );
+};
+
+export default Paper;
