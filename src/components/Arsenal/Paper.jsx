818865a706b09f6838ef5e786e1fcab0d1682945
Kalizar <kalinangelov@abv.bg>
2024-05-19 18:46:53 +0300

    Update Paper.jsx

--- src/components/Arsenal/Paper.jsx
+++ src/components/Arsenal/Paper.jsx
@@ -2,7 +2,7 @@ const Paper = ({ onSelect }) => {
   return (
     <button onClick={ () => onSelect("paper") } id="weapon-of-war-paper" className="weapon-btn" value="paper">
       <figure id="paper-container" className="weapon-container">
-        <img src="/public/images/icon-paper.svg" alt="paper" />
+        <img src="/images/icon-paper.svg" alt="paper" />
       </figure>
     </button>
   );
