1221e9cf14736d17c209c8e42106b4df759914d7
Kalizar <kalinangelov@abv.bg>
2024-05-19 18:41:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import "./App.css";
 
-import Home from "./components/Home";
-import Rules from "./components/Rules";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import Rules from "./components/Rules/Rules";
+
+import { Context } from "./context/Context";
+import Arena from "./components/Arena/Arena";
 
 function App() {
-  return (
-    <>
-     <Rules />
-    </>
-  )
-}
+  const [location, setLocation] = useState("home");
+  const [selected, setSelected] = useState('');
+  const [score, setScore] = useState(12);
 
-export default App
+  const contextValue = {
+    location,
+    setLocation,
+    selected,
+    setSelected,
+    score,
+    setScore
+  };
+
+  return (
+    <Context.Provider value={contextValue} >
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/arena" element={<Arena />} />
+          <Route path="/rules" element={<Rules />} />
+        </Routes>
+      </>
+    </Context.Provider>
+  );
+};
+
+export default App;
