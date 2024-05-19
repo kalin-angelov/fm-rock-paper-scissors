61d3196b627ce3e8a343a1f46837463dc5c246bc
Kalizar <kalinangelov@abv.bg>
2024-05-19 18:47:16 +0300

    Update Scissors.jsx

--- src/components/Arsenal/Scissors.jsx
+++ src/components/Arsenal/Scissors.jsx
@@ -2,7 +2,7 @@ const Scissors = ({ onSelect }) => {
   return (
     <button onClick={ () => onSelect("scissors") } id="weapon-of-war-scissors" className="weapon-btn">
         <figure id="scissors-container" className="weapon-container">
-            <img src="/public/images/icon-scissors.svg" alt="scissors" />
+            <img src="/images/icon-scissors.svg" alt="scissors" />
         </figure>
     </button>
   );
