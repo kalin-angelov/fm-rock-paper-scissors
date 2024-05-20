55ff2722762445b8d6c92c1ac9cd48465fed9da5
Kalizar <kalinangelov@abv.bg>
2024-05-20 15:09:47 +0300

    Update Arena.jsx

--- src/components/Arena/Arena.jsx
+++ src/components/Arena/Arena.jsx
@@ -6,13 +6,78 @@ import RulesBtn from "../Rules/RulesBtn";
 
 import { Context } from "../../context/Context";
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Arena = () => {
   const navigate = useNavigate();
-  const { selected, setLocation } = useContext(Context);
-  const [result, setResult] = useState("You Lose");
+  const { selected, score, setScore } = useContext(Context);
+  const [result, setResult] = useState("");
+  const [houseHand, setHouseHand] = useState("");
+
+  const lose = "You Lose";
+  const won = "You Won";
+  const draw = "Draw";
+
+  const noPoint = score + 0;
+  const wonPoint = score + 1;
+  const losePoint = (score - 1 < 0) ? 0 : score - 1;
+
+  useEffect(() => {
+    setTimeout(() => {
+      const houseChoice = Math.floor(Math.random() * 3) + 1;
+      
+      if (houseChoice === 1) {
+        setHouseHand(<Paper />);
+        switch (selected) {
+          case "paper":
+            setResult(draw);
+            setScore(noPoint);
+            break;
+          case "rock":
+            setResult(lose);
+            setScore(losePoint);
+            break;
+          case "scissors":
+            setResult(won);
+            setScore(wonPoint);
+            break;
+        }
+      } else if (houseChoice === 2){
+        setHouseHand(<Scissors />);
+        switch (selected) {
+          case "scissors":
+            setResult(draw);
+            setScore(noPoint);
+            break;
+          case "paper":
+            setResult(lose);
+            setScore(losePoint);
+            break;
+          case "rock":
+            setResult(won);
+            setScore(wonPoint);
+            break;
+        }
+      } else {
+        setHouseHand(<Rock />);
+        switch (selected) {
+          case "rock":
+            setResult(draw);
+            setScore(noPoint);
+            break;
+          case "scissors":
+            setResult(lose);
+            setScore(losePoint);
+            break;
+          case "paper":
+            setResult(won);
+            setScore(wonPoint);
+            break;
+        }
+      }
+    }, "2000");
+  },[]);
   
   const navigateToHome = () => {
     setResult("");
@@ -33,7 +98,11 @@ const Arena = () => {
         </div>
 
         <div className="house-choice">
-          <div className="empty"></div>
+          {houseHand === "" ? 
+            <div className="empty"></div>
+            :
+            houseHand
+          }
           <p>The House Picked</p>
         </div>
       </div>
