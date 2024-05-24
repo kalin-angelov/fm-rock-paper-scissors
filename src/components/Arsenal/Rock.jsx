f8cadcb153d13bf03947ad7406904ebf5c7a6bb1
Kalizar <kalinangelov@abv.bg>
2024-05-24 14:55:35 +0300

    Update Rock.jsx

--- src/components/Arsenal/Rock.jsx
+++ src/components/Arsenal/Rock.jsx
@@ -1,9 +1,13 @@
-const Rock = ({ onSelect }) => {
+const Rock = ({ onSelect, arena }) => {
   return (
-    <button onClick={ () => onSelect("rock") } id="weapon-of-war-rock" className="weapon-btn">
-        <figure id="rock-container" className="weapon-container">
-            <img src="/images/icon-rock.svg" alt="rock" />
-        </figure>
+    <button 
+      onClick={ () => onSelect("rock") } 
+      id="weapon-of-war-rock" 
+      className={arena ? "weapon-arena" : "weapon-btn"} 
+    >
+      <figure id="rock-container" className={arena ? "weapon-arena" : "weapon-container"}>
+        <img src="/images/icon-rock.svg" alt="rock" />
+      </figure>
     </button>
   )
 }
