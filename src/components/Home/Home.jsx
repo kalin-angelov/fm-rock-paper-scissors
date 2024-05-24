4457529773a735a20190066d2b08063dc390728c
Kalizar <kalinangelov@abv.bg>
2024-05-24 14:55:38 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,14 @@
-import { useContext } from "react";
-
 import Score from "../Score/Score";
 import Arsenal from "../Arsenal/Arsenal";
-import RulesBtn from "../Rules/RulesBtn";
-
-import { Context } from "../../context/Context";
-import Arena from "../Arena/Arena";
+import Rules from "../Rules/Rules";
 
 const Home = () => {
-  const { location} = useContext(Context);
 
   return (
     <div className="home-section">
       <Score />
-
-      {
-        location === "home" ? 
-        <Arsenal />
-        :
-        <Arena />
-      }
-      
-      <RulesBtn />
+      <Arsenal />
+      <Rules />
     </div>
   );
 };
