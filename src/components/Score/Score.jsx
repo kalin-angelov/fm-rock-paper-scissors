d2c54b806b346b311f478fd583fc3ab830bd1460
Kalizar <kalinangelov@abv.bg>
2024-05-27 13:04:32 +0300

    Update Score.jsx

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
+
 import { useNavigate } from "react-router-dom";
+
 import { Context } from "../../context/Context";
 
 const Score = () => {
     const navigate = useNavigate();
+    
     const { score } = useContext(Context);
 
     return (
